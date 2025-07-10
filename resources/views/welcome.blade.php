6d904b2d8fcd32f2e8269ce9562f130eb74e89f7
fianbiasa <fianbiasa@gmail.com>
2025-07-10 19:46:01 +0700

    Initial Push Live Project Fian: 2025-07-10 19:46:00

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Selamat Datang di Jowmail</title>
+    <title>Selamat Datang di Jowmail.com</title>
     <link href="https://fonts.googleapis.com/css?family=Inter:400,600&display=swap" rel="stylesheet">
     <style>
         body {
