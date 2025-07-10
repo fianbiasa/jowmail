a2965cf38d6e2cd5f130ed1078cdac2cf0a298df
fianbiasa <fianbiasa@gmail.com>
2025-07-10 14:52:01 +0700

    Auto-sync: 2025-07-10 14:52:01

--- database/migrations/2025_07_10_145053_create_campaign_opens_table.php
+++ database/migrations/2025_07_10_145053_create_campaign_opens_table.php
@@ -4,22 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('campaign_opens', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('campaign_id')->constrained()->onDelete('cascade');
+            $table->foreignId('subscriber_id')->constrained()->onDelete('cascade');
+            $table->timestamp('opened_at')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('campaign_opens');
