20259a4522b176cff6ab242f4063907fbfc5e855
fianbiasa <fianbiasa@gmail.com>
2025-07-11 16:25:01 +0700

    Initial Push Live Project Fian: 2025-07-11 16:25:01

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,13 @@ use App\Models\Subscriber;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/tracking/open/{campaign}/{subscriber}.gif', [TrackingController::class, 'open'])->name('tracking.open');
-Route::get('/redirect.php', [TrackingController::class, 'redirect'])->name('tracking.click');
+
+Route::get('/redirect', [TrackingController::class, 'redirect'])->name('tracking.redirect'); // ✅ ini yang dipakai
+
 Route::match(['get', 'post'], '/unsubscribe/{subscriber}', [TrackingController::class, 'unsubscribe'])->name('unsubscribe');
-Route::get('/redirect', [\App\Http\Controllers\TrackingController::class, 'redirect']);
-Route::get('/redirect', [TrackingController::class, 'redirect'])->name('tracking.redirect');
